51cfd31a4f1eda816db7209a9ea79ad2e2c09d8f
Bruno Nogueira <bruno@brunon.com.br>
2020-07-22 23:11:43 -0300

    Inclui views e controller relacionados com pedido.

--- loja/Controllers/PedidoController.cs
+++ loja/Controllers/PedidoController.cs
@@ -1,31 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using loja.Models;
+using System.Diagnostics;
 
 namespace loja.Controllers
 {
-    public class HomeController : Controller
+    public class PedidoController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Carrossel()
         {
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Carrinho()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult Cadastro()
         {
-            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
 
+        public IActionResult Resumo()
+        {
             return View();
         }
 
@@ -33,5 +34,6 @@ namespace loja.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        
     }
 }
--- loja/Startup.cs
+++ loja/Startup.cs
@@ -34,7 +34,7 @@ namespace loja
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Pedido/Error");
             }
 
             app.UseStaticFiles();
@@ -43,7 +43,7 @@ namespace loja
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
         }
     }
