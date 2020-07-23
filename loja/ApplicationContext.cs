f2d6d0bfa787f8f7a519641c894f9f0e9ac1aa08
Bruno Nogueira <bruno@brunon.com.br>
2020-07-22 23:34:58 -0300

    Inclui models e cria o ApplicationContext

---
+++ loja/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using loja.Models;
+
+namespace loja
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Produto>().HasKey(t => t.Id);
+        }
+    }
+}
---
+++ loja/Models/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace loja.Models
+{
+    public class Produto : BaseModel
+    {
+        public Produto()
+        {
+
+        }
+
+        [Required]
+        public string Codigo { get; private set; }
+        [Required]
+        public string Nome { get; private set; }
+        [Required]
+        public decimal Preco { get; private set; }
+
+        public Produto(string codigo, string nome, decimal preco)
+        {
+            this.Codigo = codigo;
+            this.Nome = nome;
+            this.Preco = preco;
+        }
+    }
+}
