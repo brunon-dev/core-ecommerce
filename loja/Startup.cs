4f689a144892addd75cb40a9fa7b1ab2675496fd
Bruno Nogueira <bruno@brunon.com.br>
2020-07-23 22:18:36 -0300

    Inclui o migrations para criação da base de dados usando o ApplicationContext e o conceito de code first

--- loja/Startup.cs
+++ loja/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace loja
 {
@@ -22,10 +23,17 @@ namespace loja
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            string connectionString = Configuration.GetConnectionString("Default");
+
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(connectionString)
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -45,6 +53,12 @@ namespace loja
                     name: "default",
                     template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
+
+            // garante que o banco de dados está criado
+            serviceProvider
+                .GetService<ApplicationContext>()
+                .Database
+                .Migrate();
         }
     }
 }
